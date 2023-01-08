9508900e7cf6a0a0616095739366f93c22618c28
Jonathan4H <joalvin2000@gmail.com>
2023-01-09 02:31:45 +0700

    Added more functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function validateData() {
-    var username = document.getElementById("username")
-    var password = document.getElementById("password")
+    var username = document.getElementsByName("username")
+    var password = document.getElementsByName("password")
 
     if(username.value.length < 5) alert("Username must be 5 characters or more")
     else if(password.value.length < 8) alert("Password length must be 8 characters or more")
--- php/home-page.php
+++ php/home-page.php
@@ -1,4 +1,15 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: login.php');
+	exit;
+}
+?>
+
 <!DOCTYPE html>
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +19,7 @@
 <body>
     <nav class="navbar">
         <h2>Jakarta<span>Times</span></h2>
-        <a href="home-page.html">Home</a>
+        <a href="home-page.php">Home</a>
         <a href="about-page.html">About</a>
         <div class="dropdown">
             <button class="dropbtn">Categories</button>
@@ -18,7 +29,8 @@
                 <a href="#entertainment">Entertainment</a>
             </div>
         </div> 
-        <a href="login.php" id="login">Login</a>
+        <a href="profile.php">Profile</a>
+        <a href="logout.php">Logout</a>
     </nav>
     <section class="home-content">
         <a href="#trending">Trending</a>
@@ -42,3 +54,4 @@
         <h4>JakartaTimes &copy; 2023</h4>
     </footer>
 </body>
+</html>
